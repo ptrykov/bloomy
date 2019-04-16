a5c6743cbd6643744c4422441983a6db8db95a12
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2019-04-16 15:20:25 +0200

    Use BloomyConfig to define config file

--- internal/config.go
+++ internal/config.go
@@ -8,8 +8,21 @@ type ServerConfig struct {
 	Port string
 }
 
+type fileFromEnv struct {
+	BloomyConfig string
+}
+
 func ReadConfig() (c *ServerConfig) {
 	c = &ServerConfig{}
-	config.From("config/dev.config").FromEnv().To(c)
+	fe := &fileFromEnv{}
+
+	config.FromEnv().To(fe)
+
+	if fe.BloomyConfig == "" {
+		config.From("config/dev.config").FromEnv().To(c)
+	} else {
+		config.From(fe.BloomyConfig).FromEnv().To(c)
+	}
+
 	return
 }
